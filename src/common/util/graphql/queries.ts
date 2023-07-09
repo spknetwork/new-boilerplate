592f7d1baabaefa8ba3d984b5dc46496ca309e7a
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-07-09 21:22:20 +0300

    added query

--- src/common/util/graphql/queries.ts
+++ src/common/util/graphql/queries.ts
@@ -29,3 +29,33 @@ export const GET_TRENDING_FEED = gql`
     }
   }
 `;
+
+export const GET_SOCIAL_FEED = gql`
+  query socialFeed($tags: [String!]) {
+    socialFeed(feedOptions: { byTag: { _in: $tags } }) {
+      items {
+        title
+        ... on HivePost {
+          community
+        }
+        body
+        permlink
+        author {
+          username
+          profile
+        }
+        json_metadata {
+          image
+          app
+        }
+        created_at
+        updated_at
+        stats {
+          num_votes
+          num_comments
+          total_hive_reward
+        }
+      }
+    }
+  }
+`;
